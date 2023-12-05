e77a1ea246f35d0e7a5cdcc6348ca49a7076ea03
Sean Currlin <sean.currlin@gmail.com>
2023-12-05 13:30:13 -0500

    update Two_Sum answer and discussion

--- Easy/1_Two_Sum.py
+++ Easy/1_Two_Sum.py
@@ -1,30 +1,28 @@
 # 1 Pseudocode
 
-# The function `twoSum` is designed to find two numbers in an array that add up to a specific target number. It does this by using a dictionary to keep track of the numbers it has seen so far and their indices.
-
-# Here's a step-by-step breakdown:
-
-# 1. The function starts by initializing an empty dictionary `num_dict`.
-
-# 2. It then enters a loop where it iterates over the `nums` array. For each number, it does the following:
-
-#   - It calculates the difference between the target number and the current number. This difference is the number we need to find in the array to ensure that the current number and this difference add up to the target.
-
-#   - It checks if this difference is already in the dictionary. If it is, that means we have found two numbers that add up to the target. The function then returns a list containing the index of the difference (which we get from the dictionary) and the index of the current number.
-
-#   - If the difference is not in the dictionary, that means we haven't encountered the required number in the array yet. So, the function adds the current number and its index to the dictionary and continues to the next iteration.
-
-# This process continues until the function finds a pair of numbers that add up to the target, or until it has checked all the numbers in the array.
-
-# The reason this solution is efficient is that it only needs to iterate over the array once, making it a linear O(n) solution. The use of a dictionary allows for fast lookups to check if a number has been seen before.
+# 1. Initialize an empty dictionary.
+# 2. Iterate over the array with enumeration. For each index and number:
+#   - Calculate the complement of the number with respect to the target.
+#   - If the complement is in the dictionary, return the index of the complement and the current index.
+#   - Otherwise, add the number and its index to the dictionary.
+# 3. If no two numbers add up to the target, return an empty list.
 
 class Solution:
     def twoSum(self, nums, target):
-        num_dict = {}
+        num_map = {}
         for i, num in enumerate(nums):
-            if target - num in num_dict:
-                return [num_dict[target - num], i]
-            num_dict[num] = i
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []
 
 sol = Solution()
 print(sol.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
+print(sol.twoSum([3, 2, 4], 6))  # Output: [1, 2]
+
+# This method works by using a dictionary to keep track of the numbers it has seen so far and their indices.
+# For each number, it calculates the complement with respect to the target and checks if the complement is in the dictionary.
+# If it is, it returns the index of the complement and the current index.
+# Otherwise, it adds the number and its index to the dictionary.
+# If no two numbers add up to the target, it returns an empty list.
